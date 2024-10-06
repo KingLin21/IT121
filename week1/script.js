47fbf0a5b7b3abb5fb8ed82204b08c483b157bd4
KingLin21 <117612761+KingLin21@users.noreply.github.com>
2024-10-06 00:19:10 +0000

    Fixed the code to actually fit the assignment

--- week1/script.js
+++ week1/script.js
@@ -3,7 +3,3 @@ let lastName = "Linhart";
 let yearsOfStudy = 2;  
 let goal = "pursue a higher education at a 4 year university or I will join the workforce";
 
-let profileMessage = `My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} year(s) and plan to ${goal} after completing my courses.`;
-
-
-document.getElementById("profile").innerText = profileMessage
